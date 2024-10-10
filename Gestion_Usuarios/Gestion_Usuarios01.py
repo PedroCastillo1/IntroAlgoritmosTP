e8eafd22ad9db7ca6f3e354d79393fa254449573
Luquita-DG <lucadigresia01@gmail.com>
2024-10-10 18:55:40 -0400

    Gestion de usuarios

---
+++ Gestion_Usuarios/Gestion_Usuarios01.py
@@ -0,0 +1,40 @@
+Fin = -1
+
+
+
+
+Lista_Usuarios = []
+Lista_Contraseñas = []
+
+
+def Crear_Usuario()
+    
+    return
+
+def Ingreso_Usuario():
+    Usuario = str (input("Ingresar el Nombre del Usuario: "))
+    Contraseña = int (input("Ingresar la Contraseña del Usuario: "))
+    #Busqueda del Usuario si Exite
+    if():
+        
+    return 
+
+Programa = int (input("Bienvenido, Para iniciar el programa ingrese el Valor =  1, Para Apagar el programa ingrese el valor = -1"))
+
+while(Programa != Fin):
+    print("Ingrese el Valor = 1 (Si usted ya tiene creada una cuenta).")
+    print("Ingrese el valor = 2 (Si usted quiere crearse una cuenta)")
+    Valor_Cuenta = int (input("INGRESE SU RESPUESTA")) 
+
+    while((Valor_Cuenta != 1) or (Valor_Cuenta != 2))
+        print("Ingresaste mal el valor de las opciones: ERROR INGRESE NUEVAMENTE EL VALOR")
+        Valor_Cuenta = int (input("INGRESE SU RESPUESTA")) 
+
+    if(Valor_Cuenta == 1):
+        Ingreso_Usuario(Lista_Usuarios,Lista_Contraseñas)
+    elif(Valor_Cuenta == 2):
+        Crear_Usuario(Lista_Usuarios,Lista_Contraseñas)
+
+
+
+print("Hasta luego!!, (Usted apago el programa)")
