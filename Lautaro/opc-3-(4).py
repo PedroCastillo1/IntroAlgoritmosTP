4a00300a462416df24d3a1aa4a6a0b75ec97b75c
Lautaro F <160680628+Learnerwanxiety@users.noreply.github.com>
2024-10-24 15:23:58 -0300

    Update opc-3-(4).py

--- Lautaro/opc-3-(4).py
+++ Lautaro/opc-3-(4).py
@@ -5,6 +5,7 @@ stock_remeras=[1,22,3,4]
 pantalones=["Producto A","Producto B","Producto C","Producto D","Producto E",]
 stock_pantalones=[4,2,5,7]
 
+
 #El usuario debe elegir que lista desea ver.  Se debe agregar un print nuevo si se crean listas nuevas
 print("1 para ver Remeras")
 print("2 para ver pantalones")
@@ -14,34 +15,37 @@ lista_elegida=int(input("Elija que lista ver: "))
 #Elección
 # mismo caso que con los print, se agrega un nuevo elif si se crean nuevas listas
 if lista_elegida==1:
-    producto=remeras
+    prod=remeras
     stock=stock_remeras
     print("Ordenaremos la lista Remeras")
 elif lista_elegida==2:
-    producto=pantalones
+    prod=pantalones
     stock=stock_pantalones
     print("Ordenaremos la lista Pantalones")
+else:
+    print("La opción que eligió es inexistente, porfavor elija una que sea válida")
+    lista_elegida=int(input("Elija que lista ver: "))
 
-#listas para los productos ordenados
-producto_ordenado=[]
-stock_ordenado=[]
+#crea la lista de tupla de forma manual
+tupla=[]
+def crear_tupla(tuplax, producto, stonck):
+    for i in range(len(stonck)):
+        tuplax.append((producto[i], stonck[i]))
 
+#ordena la lista elegida
+def ordenar(arr): 
+    c=len(arr)
+    for a in range(c):
+        for j in range(0, c-a-1):
+            if arr[j]>arr[j+1]:
+                arr[j], arr[j+1]=arr[j+1], arr[j]
 
-#ordenar lista elegida
-
-index_menor_stock=0
-while stock:
-    for i in range(len(stock)):
-        if stock[i]< stock[index_menor_stock]:
-            index_menor_stock=i
-
-#agregar a la lista ordenada 
-producto_ordenado.append(producto[index_menor_stock])
-stock_ordenado.append(stock[index_menor_stock])
-
-
-print("Productos ordenados en base al stock: ")
-for a in range(len(producto_ordenado)):
-    print(f"{producto_ordenado[a]}:{stock_ordenado[a]} en stock")
-
-
+ordenado=[]
+crear_tupla(tupla, prod, stock)
+ordenar(tupla)
+#muestra la lista ordenada
+for i in range(len(tupla)):
+    ordenado.append(tupla[i])
+ordenar(ordenado)
+print("\nlista ordenada")
+print(ordenado)
